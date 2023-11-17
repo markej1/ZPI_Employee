e88ec9b115e32f07cd907ce73b08d460edaa5b3c
markej1 <xmjarx@gmail.com>
2023-11-17 18:42:50 +0100

    file browser button

--- src/app/components/program/program.component.ts
+++ src/app/components/program/program.component.ts
@@ -10,8 +10,34 @@ export class ProgramComponent {
 
     files: NgxFileDropEntry[] = [];
 
+    chosenFile: File | null = null;
+    chosenFileName?: string;
+
+    availableSections: boolean[] = [];
+    actualSection?: number;
+
+    constructor() {
+        this.availableSections = Array(4).fill(true);
+    }
+
     dropFile(files: NgxFileDropEntry[]) {
         console.log(files);
+    }
+
+    openBrowser(index: number) {
+        if (this.availableSections[index]) {
+            const file = document.getElementById('input_file_' + index)!;
+            file.click();
+            this.actualSection = index;
+        }
+    }
+
+    chooseFile(event: any) {
+        this.chosenFile = event.target.files[0];
+        if (this.chosenFile) {
+            this.chosenFileName = this.chosenFile.name;
+            this.availableSections[this.actualSection!] = false;
+        }
     }
 
 }
