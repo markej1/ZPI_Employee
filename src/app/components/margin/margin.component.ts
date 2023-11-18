1fa43e7aa7980847de3b10e6f6ba0b17688ea208
markej1 <xmjarx@gmail.com>
2023-11-18 18:49:19 +0100

    logout button design

--- src/app/components/margin/margin.component.ts
+++ src/app/components/margin/margin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-margin',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./margin.component.css']
 })
 export class MarginComponent {
+
+    constructor(private router: Router) {
+    }
+
+    logout() {
+        this.router.navigateByUrl("/");
+    }
 
 }
