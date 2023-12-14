babe6493a9a22b36001547ca523645fa53f51cfb
Karolina Bryłka <karolina.brylka88@gmail.com>
2023-12-14 13:51:58 +0100

    added program list

--- src/app/services/programs-data-source.service.ts
+++ src/app/services/programs-data-source.service.ts
@@ -1,14 +1,16 @@
 import {Inject, inject, Injectable} from '@angular/core';
 import {CollectionViewer, DataSource} from "@angular/cdk/collections";
-import {Observable, ReplaySubject} from "rxjs";
+import {catchError, Observable, ReplaySubject} from "rxjs";
 import {Program} from "../model/program";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProgramsDataSourceService extends DataSource<Program> {
     private _dataStream = new ReplaySubject<Program[]>();
-    constructor() {
+    private _url = 'https://susel.pythonanywhere.com';
+    constructor(private _httpClient: HttpClient) {
         super();
     }
     connect(collectionViewer: CollectionViewer): Observable<Program[]> {
@@ -20,5 +22,17 @@ export class ProgramsDataSourceService extends DataSource<Program> {
 
     setData(data: Program[]) {
         this._dataStream.next(data);
+    }
+
+    getAllPrograms(): Observable<Program[]> {
+        return this._httpClient.get<Program[]>(`${this._url}/list-programs/`)
+    }
+
+    deleteProgram(level: string, name: string, cycle: string, specialization: string) {
+        if(specialization === undefined) {
+            return this._httpClient.delete<string>(`${this._url}/delete-program/${level}/${name}/${cycle}/`)
+        } else {
+            return this._httpClient.delete<string>(`${this._url}/delete-program/${level}/${name}/${cycle}/${specialization}/`)
+        }
     }
 }
